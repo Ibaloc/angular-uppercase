70f3021e690bbe4809a94262a430130c0e9a8f49
Ibaloc <124224586+Ibaloc@users.noreply.github.com>
2023-02-14 11:15:45 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,19 @@ import { Component, ElementRef } from '@angular/core';
 })
 export class AppComponent {
   name = 'Angular Exercise';
-}
+  result = '';
 
-var alpha = 'ABC';
-var beta = 'yxz';
-var result = '';
+  alpha = 'ABC';
+  beta = 'yxz';
 
-for (let i = 0; i < alpha.length; i++) {
-  for (let j = 0; j < beta.length; j++) {
-    result = result + '"' + alpha[i] + beta[j] + '"' + ', ';
+  ngOnInit() {
+    var alpha = this.alpha;
+    var beta = this.beta;
+
+    for (let i = 0; i < alpha.length; i++) {
+      for (let j = 0; j < beta.length; j++) {
+        this.result = this.result + '"' + alpha[i] + beta[j] + '"' + ', ';
+      }
+    }
   }
 }

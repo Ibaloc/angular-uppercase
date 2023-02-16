d5619b7f9118eee72962d94fad49a88def7f74d3
Ibaloc <124224586+Ibaloc@users.noreply.github.com>
2023-02-16 11:38:52 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,22 @@ export class AppComponent {
   final = '';
   enterednumber = '3';
   checkIsPEO = '';
+  checkIsPEO_M = '';
   numberpalindrome = 'daa';
   checkIsPalindrome = '';
   3;
   ngOnInit() {
     this.JavaSciptRandom();
     this.checkIsPalindrome = this.isPalindrome(this.numberpalindrome);
-    this.isPrimeEvenOdd(this.enterednumber);
+    this.isPrimeEvenOdd(this.enterednumber, 'S');
+
+    this.isPrimeEvenOdd(0, 'M');
+    this.isPrimeEvenOdd(1, 'M');
+    this.isPrimeEvenOdd(1, 'M');
+    this.isPrimeEvenOdd(2, 'M');
+    this.isPrimeEvenOdd(3, 'M');
+    this.isPrimeEvenOdd(5, 'M');
+    this.isPrimeEvenOdd(8, 'M');
   }
 
   JavaSciptRandom() {
@@ -36,32 +45,56 @@ export class AppComponent {
     this.result = this.final.substring(0, this.final.length - 2);
   }
 
-  isPrimeEvenOdd(number) {
+  isPrimeEvenOdd(number, type) {
     if (number == null) {
       this.checkIsPEO = 'Enter a number';
     } else {
-      this.checkIsPEO = '';
-      var result = '';
-      if (number <= 1) result = 'Not Prime';
-      if (number == 2) result = 'Prime';
-      var limit = Math.floor(Math.sqrt(number));
-      for (let i = 2; i <= limit; ++i) {
-        if (number % i == 0) {
-          result = 'Not Prime';
-        } else {
+      if (type == 'S') {
+        this.checkIsPEO = '';
+        var result = '';
+        if (number <= 1) result = 'Not Prime';
+        if (number == 2) result = 'Prime';
+        var limit = Math.floor(Math.sqrt(number));
+        for (let i = 2; i <= limit; ++i) {
+          if (number % i == 0) {
+            result = 'Not Prime';
+          } else {
+            result = 'Prime';
+          }
+        }
+        if (result == '') {
           result = 'Prime';
         }
-      }
-      if (result == '') {
-        result = 'Prime';
-      }
-      if (number % 2 == 0) {
-        result = result + '/Even';
-      } else {
-        result = result + '/Odd';
-      }
+        if (number % 2 == 0) {
+          result = result + '/Even';
+        } else {
+          result = result + '/Odd';
+        }
 
-      this.checkIsPEO = result;
+        this.checkIsPEO = result;
+      } else {
+        var result = '';
+        if (number <= 1) result = 'Not Prime';
+        if (number == 2) result = 'Prime';
+        var limit = Math.floor(Math.sqrt(number));
+        for (let i = 2; i <= limit; ++i) {
+          if (number % i == 0) {
+            result = 'Not Prime';
+          } else {
+            result = 'Prime';
+          }
+        }
+        if (result == '') {
+          result = 'Prime';
+        }
+        if (number % 2 == 0) {
+          result = result + '/Even';
+        } else {
+          result = result + '/Odd';
+        }
+
+        this.checkIsPEO_M = this.checkIsPEO_M + number + ' - ' + result + ' ';
+      }
     }
   }
 
@@ -71,7 +104,7 @@ export class AppComponent {
   }
 
   checkValue() {
-    this.isPrimeEvenOdd(this.enterednumber);
+    this.isPrimeEvenOdd(this.enterednumber, 'S');
   }
 
   checkPalindrome() {
